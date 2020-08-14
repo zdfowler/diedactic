0124f11a312ca68d41346446e337d4a6a198c037
Zac Fowler <zfowler_uno@hotmail.com>
2020-08-14 04:32:21 -0500

    Add summarized data file check

--- run-summarize-data.sh
+++ run-summarize-data.sh
@@ -1,17 +1,26 @@
 source ./env
 
+if [[ -f summarized-data.csv ]]; then
+	echo "Summarized data detected from previous run. File will be removed if you conintue."
+	read -e -p "Continue [y/N]? " choice
+	[[ "$choice" == [Nn]* ]] && echo "Selected No. Exiting." && exit
+
+	echo "Selected Yes; continuing..."
+	rm summarized-data.csv
+
+fi
+
 while read image; do
+	SIZE1=`docker images --format '{{.Repository}},{{.Size}}' $LOCAL_REGISTRY/$image`
+	OM1=`jq '.LayerCount,.Vulnerabilities.Negligible, .Vulnerabilities.Low, .Vulnerabilities.Medium , .Vulnerabilities.High , .Vulnerabilities.Negligible+.Vulnerabilities.Low+.Vulnerabilities.Medium+.Vulnerabilities.High | length ' logs/$image.clair.json | xargs | sed 's/ /,/g'`
 
-SIZE1=`docker images --format '{{.Repository}},{{.Size}}' $LOCAL_REGISTRY/$image`
-OM1=`jq '.LayerCount,.Vulnerabilities.Negligible, .Vulnerabilities.Low, .Vulnerabilities.Medium , .Vulnerabilities.High , .Vulnerabilities.Negligible+.Vulnerabilities.Low+.Vulnerabilities.Medium+.Vulnerabilities.High | length ' logs/$image.clair.json | xargs | sed 's/ /,/g'`
+	# Ensure the log file can be parsed by jq if no image was found
+	#   A log file wilil contain the string below, but not in valid JSON, if the slimming did not create an image.
+	sed -i "s/^Can't pull fsLayers/[\"Can't pull fsLayers\"]/g" logs/$image.clair.slim.json
 
-sed -i "s/^Can't pull fsLayers/[\"Can't pull fsLayers\"]/g" logs/$image.clair.slim.json
+	SIZE2=`docker images --format '{{.Repository}},{{.Size}}' $LOCAL_REGISTRY/$image.slim`
+	OM2=`jq '.LayerCount?,.Vulnerabilities?.Negligible, .Vulnerabilities?.Low, .Vulnerabilities?.Medium , .Vulnerabilities?.High , .Vulnerabilities?.Negligible +.Vulnerabilities?.Low + .Vulnerabilities?.Medium + .Vulnerabilities?.High | length ' $LOGS/$image.clair.slim.json | xargs | sed 's/ /,/g'`
 
-SIZE2=`docker images --format '{{.Repository}},{{.Size}}' $LOCAL_REGISTRY/$image.slim`
-OM2=`jq '.LayerCount?,.Vulnerabilities?.Negligible, .Vulnerabilities?.Low, .Vulnerabilities?.Medium , .Vulnerabilities?.High , .Vulnerabilities?.Negligible +.Vulnerabilities?.Low + .Vulnerabilities?.Medium + .Vulnerabilities?.High | length ' $LOGS/$image.clair.slim.json | xargs | sed 's/ /,/g'`
-
-
-
-echo $image,$SIZE1,$OM1,$SIZE2,$OM2
+	echo $image,$SIZE1,$OM1,$SIZE2,$OM2
 
 done < $IMAGES_FILE
